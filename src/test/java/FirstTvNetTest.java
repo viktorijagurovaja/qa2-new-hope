de58fab67d5b8328f16686402d9bce6cf2ef405b
Viktorija Gurovaja <viktorijagurovaja@gmail.com>
2021-04-28 21:14:46 +0300

    Initial commit

---
+++ src/test/java/FirstTvNetTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FirstTvNetTest {
+    private final By LOCATOR_BY_ID = By.id("elementId");
+    private final By LOCATOR_BY_NAME = By.name("elementName");
+    private final By LOCATOR_BY_TAGNAME = By.tagName("nameOfTag");
+    private  final By LOCATOR_BY_CLASS = By.className("elementClassName");
+    private  final By LOCATOR_BY_XPATH = By.xpath(".//*[contains(@class,'list-article__meta-info flex')]");
+
+
+    private  final By ACCEPT_COOKIES_BTN = By.xpath(".//button[@mode = 'primary']");
+
+
+    @Test
+    public void firstTest() {
+        //WebDriver = browser window
+        System.setProperty("webdriver.chrome.driver", "c://IdeaProjects/chromedriver.exe");
+        WebDriver browserWindow = new ChromeDriver();
+        browserWindow.manage().window().maximize();
+        browserWindow.get("http://tvnet.lv");
+
+//      Thread.sleep(1000);
+
+        WebDriverWait wait = new WebDriverWait(browserWindow, 10);
+        WebDriverWait mailWait = new WebDriverWait(browserWindow, 60, 2000);
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(ACCEPT_COOKIES_BTN));
+
+        WebElement acceptBtn = browserWindow.findElement(ACCEPT_COOKIES_BTN);
+        acceptBtn.click();
+
+//      WebElement acceptBtn = browserWindow.find.Element(ACCEPT_COOKIES_BTN);
+//      acceptBtn.click();
+
+//      browserWindow.findElement(ACCEPT_COOKIES_BTN).click();
+
+    }
+}
